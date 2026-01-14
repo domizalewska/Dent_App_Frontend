452ada941c306445db3290c0914e703a73ccc03d
Dominika <domzal1801@gmail.com>
2026-01-14 20:32:47 +0100

    feat: Added new configuration of files to typescript

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,37 +1,55 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt({
-    extends: ['plugin:prettier/recommended'],
-    plugins: ['prettier'],
-    files: [
-        '.nuxt/**/*.{ts,js,vue}',
-        'src/**/*.{ts,js,vue}'
+  extends: ['plugin:prettier/recommended', 'eslint:recommended', 'plugin:vue/vue3-recommended'],
+  plugins: ['prettier'],
+  files: ['.nuxt/**/*.{ts,js,vue}', 'src/**/*.{ts,js,vue}'],
+  ignores: ['node_modules/**', 'dist/**', '.nuxt/**', 'tests/e2e/**'],
+  rules: {
+    'prettier/prettier': 'warn',
+    'vue/html-self-closing': [
+      'warn',
+      {
+        html: {
+          void: 'never',
+          normal: 'never',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
+      },
     ],
-    ignores: [
-        'node_modules/**',
-        'dist/**',
-        '.nuxt/**',
-        'tests/e2e/**'
+    'vue/attributes-order': [
+      'error',
+      {
+        order: [
+          'DEFINITION',
+          'LIST_RENDERING',
+          'CONDITIONALS',
+          'RENDER_MODIFIERS',
+          'GLOBAL',
+          ['UNIQUE', 'SLOT'],
+          'TWO_WAY_BINDING',
+          'OTHER_DIRECTIVES',
+          'OTHER_ATTR',
+          'EVENTS',
+          'CONTENT',
+        ],
+        alphabetical: false,
+        sortLineLength: false,
+      },
     ],
-    rules: {
-        'prettier/prettier': 'warn',
-        'vue/html-self-closing': ['warn', {
-            "html": {
-                "void": 'never',
-                "normal": 'never',
-                "component": 'always'
-            },
-            "svg": 'always',
-            "math": 'always'
-        }],
-        'vue/html-closing-bracket-newline': ['warn', { singleline: 'never', multiline: 'always' }],
-        'vue/html-closing-bracket-spacing': ['warn', { startTag: 'never', endTag: 'never', selfClosingTag: 'always' }],
-        'semi': ['warn',  'never'],
-        'quotes': ['warn', 'single', { avoidEscape: true }],
-        'comma-dangle': ['warn', 'only-multiline'],
-        'no-console': ['warn'],
-        'no-debugger': ['warn'],
-        "vue/component-definition-name-casing": ["error", "PascalCase" ]
-    }
-})
+    'vue/html-closing-bracket-newline': ['warn', { singleline: 'never', multiline: 'always' }],
+    'vue/html-closing-bracket-spacing': [
+      'warn',
+      { startTag: 'never', endTag: 'never', selfClosingTag: 'always' },
+    ],
+    semi: ['warn', 'never'],
+    quotes: ['warn', 'single', { avoidEscape: true }],
+    'comma-dangle': ['warn', 'only-multiline'],
+    'no-console': ['warn'],
+    'no-debugger': ['warn'],
+    'vue/component-definition-name-casing': ['error', 'PascalCase'],
+  },
+});
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,28 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
   modules: [
     '@nuxt/eslint',
     '@nuxt/scripts',
     '@nuxt/test-utils',
-    '@nuxt/ui'
-  ]
-})
+    '@nuxt/ui',
+    '@nuxtjs/stylelint-module',
+  ],
+  app: {
+    head: {
+      title: 'Dent App Frontend',
+      meta: [{ name: 'description', content: 'Frontend aplikacji dentystycznej' }],
+      link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
+    },
+  },
+  typescript: {
+    strict: true,
+    shim: false,
+  },
+  css: ['@/assets/main.css'],
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.BASE_URL || '/api',
+    },
+  },
+});
