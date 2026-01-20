10753683ff96cead7a56338065896d5c8d27a04b
Dominika <domzal1801@gmail.com>
2026-01-20 21:14:52 +0100

    wip: LoginForm

--- app/pages/login/index.vue
+++ app/pages/login/index.vue
@@ -3,7 +3,7 @@ import LoginForm from '~/components/login-03/components/LoginForm.vue';
 </script>
 
 <template>
-  <div class="flex min-h-svh flex-row items-center justify-between gap-6 p-6 md:p-10">
+  <div class="flex flex-row items-center justify-between gap-6 p-6 md:p-10">
     <div class="flex w-full flex-row flex-wrap items-center justify-center"></div>
     <div class="flex w-full max-w-md max-h-md flex-col">
       <LoginForm />
