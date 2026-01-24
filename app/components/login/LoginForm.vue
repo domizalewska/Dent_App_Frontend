83fc468955aa7a63a95062a60522d7203577b014
Dominika <domzal1801@gmail.com>
2026-01-24 19:50:57 +0100

    fix: Fixed LoginForm.vue style on page

--- app/components/login/LoginForm.vue
+++ app/components/login/LoginForm.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import type { HTMLAttributes } from 'vue';
-import { cn } from '@/lib/utils';
-import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { cn } from '~/lib/utils';
+import { Button } from '~/components/ui/button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '~/components/ui/card';
 import {
   Field,
   FieldDescription,
   FieldGroup,
   FieldLabel,
   FieldSeparator,
-} from '@/components/ui/field';
-import { Input } from '@/components/ui/input';
+} from '~/components/ui/field';
+import { Input } from '~/components/ui/input';
 </script>
 
 <template>
--- app/pages/login/index.vue
+++ app/pages/login/index.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-import LoginForm from '~/components/login-03/components/LoginForm.vue';
+import LoginForm from '~/components/login/LoginForm.vue';
 </script>
 
 <template>
   <div class="flex min-h-screen justify-between bg-[url(/gradient.jpg)] bg-cover bg-center">
-    <div class="flex h-screen items-center justify-center p-12">
+    <div class="flex h-screen items-center justify-center pl-16">
       <div class="flex flex-col items-start justify-center">
         <h3 class="text-xl font-semibold">Twoje nowe hasło</h3>
         <span class="text-sm text-gray-500">Tutaj jest przykładowy tekst</span>
       </div>
     </div>
-    <div class="flex w-1/2 pr-12 pt-12 pb-12 flex-col">
+    <div class="flex w-1/3 pr-20 pt-12 pb-12 flex-col">
       <LoginForm class="flex-1" />
     </div>
   </div>
