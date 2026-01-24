b294097da11523062df0df8486e346504a0ed82b
Dominika <domzal1801@gmail.com>
2026-01-24 19:52:08 +0100

    feat: Created new component ResetPasswordForm.vue

---
+++ app/components/reset-password/ResetPasswordForm.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue';
+import { cn } from '~/lib/utils';
+import { Button } from '~/components/ui/button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '~/components/ui/card';
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSeparator,
+} from '~/components/ui/field';
+import { Input } from '~/components/ui/input';
+</script>
+
+<template>
+  <Card class="flex justify-center border-0 shadow-2xl">
+    <CardHeader class="text-center items-center">
+      <CardTitle class="flex text-xl pt-6"> Reset Hasła </CardTitle>
+      <CardDescription class="flex text-xs pb-3"> Wpisz swoje nowe hasło </CardDescription>
+    </CardHeader>
+    <CardContent>
+      <form>
+        <FieldGroup>
+          <Field>
+            <FieldLabel for="password"> Hasło </FieldLabel>
+            <Input id="hasło" type="password" required />
+          </Field>
+          <Field>
+            <div class="flex items-center">
+              <FieldLabel for="password"> Powtórz hasło </FieldLabel>
+            </div>
+            <Input id="password" type="password" required />
+          </Field>
+          <Field>
+            <Button type="submit"> Resetuj hsało </Button>
+          </Field>
+        </FieldGroup>
+      </form>
+    </CardContent>
+  </Card>
+</template>
---
+++ app/pages/reset-password/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex min-h-screen justify-between bg-[url(/gradient.jpg)] bg-cover bg-center">
+    <div class="flex h-screen items-center justify-center pl-16">
+      <div class="flex flex-col items-start justify-center">
+        <h3 class="text-xl font-semibold">Twoje nowe hasło</h3>
+        <span class="text-sm text-gray-500">Tutaj jest przykładowy tekst</span>
+      </div>
+    </div>
+    <div class="flex w-1/3 pr-20 pt-12 pb-12 flex-col">
+      <ResetPasswordForm class="flex-1" />
+    </div>
+  </div>
+</template>
