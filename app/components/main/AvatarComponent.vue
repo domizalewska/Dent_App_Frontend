e2faec50231de03f4383653e29e41181eb8febff
Dominika <domzal1801@gmail.com>
2026-01-25 00:18:29 +0100

    feat: Created components for dashboards display

---
+++ app/components/main/AvatarComponent.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+</script>
+
+<template>
+  <div class="flex flex-row flex-wrap items-center gap-12">
+    <Avatar>
+      <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+      <AvatarFallback>CN</AvatarFallback>
+    </Avatar>
+  </div>
+</template>
---
+++ app/pages/dashboard/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import SidebarComponent from '~/components/main/SidebarComponent.vue';
+import { ArrowUpIcon } from 'lucide-vue-next';
+</script>
+
+<template>
+  <div class="flex h-screen">
+    <SidebarComponent />
+    <div class="flex w-full flex-1 pt-4 pr-4">
+      <div class="flex gap-2 items-start">
+        <Button variant="outline" size="icon" class="rounded-full"><ArrowUpIcon /> </Button>
+        <AvatarComponent class="flex items-start" />
+      </div>
+    </div>
+  </div>
+</template>
