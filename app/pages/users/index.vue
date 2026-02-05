37fcea2da5eadf36f3f64cc6ca0395130cc6626f
Dominika <domzal1801@gmail.com>
2026-02-05 22:58:44 +0100

    feat: Added Datatable to user page view

--- app/pages/users/index.vue
+++ app/pages/users/index.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
+import { columns } from '~/components/ui/data-table/columns';
+import DataTable from '~/components/ui/data-table/data-table.vue';
+import { generateMockUsers } from '~/mock/users/mockUsers';
+import type { User } from '~/types/users/typesUsers';
+
 definePageMeta({
   layout: 'dashboard',
+});
+
+const data = ref<User[]>([]);
+
+async function getData(): Promise<User[]> {
+  return generateMockUsers(10);
+}
+
+onMounted(async () => {
+  data.value = await getData();
 });
 </script>
 
 <template>
-  <div class="flex h-screen"></div>
+  <div class="flex h-screen w-full">
+    <DataTable :columns="columns" :data="data" />
+  </div>
 </template>
