065063ba46cc3238933b4e33fc2081e456dc9296
Dominika <domzal1801@gmail.com>
2026-02-07 17:57:30 +0100

    feat: Created function to return custom format date

---
+++ app/utils/formatDate.ts
@@ -0,0 +1,6 @@
+import { format } from 'date-fns';
+
+export function formatDateToString(date: Date, customFormat: string): string {
+  if (!date) return 'Brak daty';
+  return format(date, customFormat);
+}
