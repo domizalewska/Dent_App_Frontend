c20288fae281d94ffdd705e9501f08a8811ea8ef
Dominika <domzal1801@gmail.com>
2026-02-08 23:03:57 +0100

    feat: Added useBreadcrumbs file

---
+++ app/composables/useBreadcrumbs.ts
@@ -0,0 +1,20 @@
+import { ref } from 'vue';
+
+interface Crumb {
+  name: string;
+  link?: string;
+}
+
+export const breadcrumbs = ref<Crumb[]>([]);
+
+export function useBreadcrumbs() {
+  function set(items: Crumb[]) {
+    breadcrumbs.value = items;
+  }
+
+  function reset() {
+    breadcrumbs.value = [];
+  }
+
+  return { breadcrumbs, set, reset };
+}
