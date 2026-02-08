b713ae0b1b5436955d1f06421c773d89af50084d
Dominika <domzal1801@gmail.com>
2026-02-08 23:04:28 +0100

    feat: Created new component for breadcrumbs

---
+++ app/components/main/BaseBreadcrumb.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface Props {
+  name: string;
+  link: string;
+}
+
+defineProps<Props>();
+</script>
+<template>
+  <Breadcrumb>
+    <BreadcrumbList>
+      <BreadcrumbItem class="hidden md:block">
+        <BreadcrumbLink :href="link"> {{ name }} </BreadcrumbLink>
+      </BreadcrumbItem>
+    </BreadcrumbList>
+  </Breadcrumb>
+</template>
