9037027a66241b5d41cead17ed39dba42fecd7b9
Dominika <domzal1801@gmail.com>
2026-02-09 21:28:48 +0100

    feat: Created profile page of logged user

---
+++ app/pages/profile/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useHeader } from '~/composables/useHeader';
+import { useBreadcrumbs } from '~/composables/useBreadcrumbs';
+
+definePageMeta({
+  layout: 'dashboard',
+});
+
+const { setHeader, resetHeader } = useHeader();
+
+resetHeader();
+setHeader('Profile');
+
+const { set } = useBreadcrumbs();
+
+set([{ name: 'Profile', link: '/profile' }]);
+</script>
+
+<template></template>
