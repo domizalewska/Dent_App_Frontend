679cd8967e4348ebd7a1642372b6bb07f93f2a46
Dominika <domzal1801@gmail.com>
2026-02-12 21:18:39 +0100

    feat: Created base components

---
+++ app/components/base/buttons/BaseAcceptDeclineButtons.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+interface Props {
+  acceptTitle?: string;
+  declineTitle?: string;
+}
+
+defineProps<Props>();
+</script>
+<template>
+  <div class="flex flex-wrap items-center gap-2 md:flex-row">
+    <Button variant="outline" >
+      <span class="text-color font-medium text-xl">{{ declineTitle }}</span>
+    </Button>
+    <Button variant="outline" >
+      <span class="text-color font-medium text-xl">{{ declineTitle }}</span>
+    </Button>
+  </div>
+</template>
---
+++ app/components/base/card/BaseCard.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+  cardTitle?: string;
+  cardDescription?: string;
+  cardButtons?: boolean;
+}>();
+</script>
+<template>
+  <Card>
+    <CardHeader >
+      <CardTitle >{{ cardTitle }}</CardTitle>
+      <CardDescription >
+        {{ cardDescription }}
+      </CardDescription>
+    </CardHeader>
+    <CardContent class="grid gap-6">
+      <slot />
+    </CardContent>
+    <CardFooter >
+      <BaseAcceptDeclineButtons v-if="cardButtons" />
+    </CardFooter>
+  </Card>
+</template>
