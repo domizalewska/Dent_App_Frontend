3de779537bd452e319b21a2d84e3fd4345f365b3
Dominika <domzal1801@gmail.com>
2026-02-12 21:19:11 +0100

    feat: Created component for header and profile tabs

---
+++ app/components/profile/tabs/ProfileTabs.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts"></script>
+<template>
+  <script setup lang="ts">
+    import { Button } from '@/components/ui/button';
+    import {
+      Card,
+      CardContent,
+      CardDescription,
+      CardFooter,
+      CardHeader,
+      CardTitle,
+    } from '@/components/ui/card';
+    import { Input } from '@/components/ui/input';
+    import { Label } from '@/components/ui/label';
+    import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+  </script>
+
+  <template>
+    <div class="flex w-full max-w-sm flex-col gap-6">
+      <Tabs default-value="profile">
+        <TabsList>
+          <TabsTrigger value="profilr"> Account </TabsTrigger>
+          <TabsTrigger value="password"> Password </TabsTrigger>
+        </TabsList>
+        <TabsContent value="profile">
+          <slot />
+        </TabsContent>
+      </Tabs>
+    </div>
+  </template>
+</template>
