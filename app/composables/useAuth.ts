145ac6ecd9445f2c6d74597c580689d7e4b33ebf
Dominika <domzal1801@gmail.com>
2026-02-16 21:49:56 +0100

    feat: Created composable to identify User Token

---
+++ app/composables/useAuth.ts
@@ -0,0 +1,11 @@
+export const useAuth = () => {
+
+  const isAuthenticated = ref<boolean>(false);
+  const token = ref();
+  if(import.meta.client) {
+    token.value = localStorage.getItem('token');
+    isAuthenticated.value = !!token.value;
+  }
+
+  return {isAuthenticated, token};
+}
