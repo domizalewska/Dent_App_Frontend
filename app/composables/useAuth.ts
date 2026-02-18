8b1657f884b11bfe137aca7e86501cb7f65c551d
Dominika <domzal1801@gmail.com>
2026-02-18 21:07:25 +0100

    fix: Fixed files form workflow check

--- app/composables/useAuth.ts
+++ app/composables/useAuth.ts
@@ -1,11 +1,10 @@
 export const useAuth = () => {
-
   const isAuthenticated = ref<boolean>(false);
   const token = ref();
-  if(import.meta.client) {
+  if (import.meta.client) {
     token.value = localStorage.getItem('token');
     isAuthenticated.value = !!token.value;
   }
 
   return isAuthenticated.value;
-}
+};
