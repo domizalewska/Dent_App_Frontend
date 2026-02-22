2bdbc27b8f6c8b103f6cf583d8c2d961e3048e6f
Dominika <domzal1801@gmail.com>
2026-02-23 00:06:18 +0100

    fix: Changed tabs view

--- app/components/profile/tabs/ProfileTabs.vue
+++ app/components/profile/tabs/ProfileTabs.vue
@@ -3,26 +3,18 @@ import { Bell, Lock, User } from 'lucide-vue-next'
 </script>
 
 <template>
-  <div class="flex w-full max-w-sm flex-col gap-6 justify-center">
-    <Tabs default-value="profile">
-      <TabsList variant="line" class="text-muted-foreground rounded-lg w-full gap-6 p-0">
-        <div class="flex flex-row w-full justify-center items-center">
-          <div class="flex flex-wrap items-center gap-2 md:flex-row">
-            <Button variant="outline" value="profil"> <User /> Profil </Button>
-          </div>
-          <div class="flex flex-wrap items-center gap-2 md:flex-row pl-4 pr-4">
-            <Button variant="outline" value="password" class="w-full pr-4 pl-4">
-              <Lock /> Hasło
-            </Button>
-          </div>
-          <Button variant="outline" value="notification" class="w-full">
-            <Bell /> Powiadomienia</Button
-          >
-        </div>
-      </TabsList>
-      <TabsContent value="profil">
-        <slot />
-      </TabsContent>
-    </Tabs>
-  </div>
+  <Tabs default-value="profile">
+    <TabsList class="w-full flex gap-2">
+      <div class="flex flex-row gap-6 w-full justify-center items-center">
+        <Button value="profile" class="inline-1/3 justify-start gap-2"> <User /> Profil </Button>
+        <Button value="password" class="flex justify-start gap-2"> <Lock /> Hasło </Button>
+        <Button value="notification" class="flex justify-start gap-2">
+          <Bell /> Powiadomienia
+        </Button>
+      </div>
+    </TabsList>
+    <TabsContent value="profil">
+      <slot />
+    </TabsContent>
+  </Tabs>
 </template>
