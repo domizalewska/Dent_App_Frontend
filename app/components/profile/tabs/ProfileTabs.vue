6cccd094be916749bbd104efe88ae18f1683c8db
Dominika <domzal1801@gmail.com>
2026-03-02 23:09:34 +0100

    fix: Fixed components with tabs

--- app/components/profile/tabs/ProfileTabs.vue
+++ app/components/profile/tabs/ProfileTabs.vue
@@ -3,18 +3,27 @@ import { Bell, Lock, User } from 'lucide-vue-next'
 </script>
 
 <template>
-  <Tabs default-value="profile">
-    <TabsList class="w-full flex gap-2">
-      <div class="flex flex-row gap-6 w-full justify-center items-center">
-        <Button value="profile" class="inline-1/3 justify-start gap-2"> <User /> Profil </Button>
-        <Button value="password" class="flex justify-start gap-2"> <Lock /> Hasło </Button>
-        <Button value="notification" class="flex justify-start gap-2">
+  <div class="w-full">
+    <Tabs default-value="profile">
+      <TabsList class="flex w-full gap-4">
+        <Button value="profile" class="w-1/3 justify-center gap-2"> <User /> Profil </Button>
+        <Button value="password" class="w-1/3 justify-center gap-2"> <Lock /> Hasło </Button>
+        <Button value="notification" class="w-1/3 justify-center gap-2">
           <Bell /> Powiadomienia
         </Button>
-      </div>
-    </TabsList>
-    <TabsContent value="profil">
-      <slot />
-    </TabsContent>
-  </Tabs>
+      </TabsList>
+      <TabsContent value="profile">
+        tetetetete
+        <slot />
+      </TabsContent>
+      <TabsContent value="password">
+        referferfererg
+        <slot />
+      </TabsContent>
+      <TabsContent value="notification">
+        wfrerer
+        <slot />
+      </TabsContent>
+    </Tabs>
+  </div>
 </template>
