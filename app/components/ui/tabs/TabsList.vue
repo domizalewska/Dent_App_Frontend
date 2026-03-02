0c2243d9212186c14fddfe7624f0feeb83c85b96
Dominika <domzal1801@gmail.com>
2026-03-02 23:09:57 +0100

    fix: Changed main component to fit others

--- app/components/ui/tabs/TabsList.vue
+++ app/components/ui/tabs/TabsList.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import type { TabsListProps } from "reka-ui"
-import type { HTMLAttributes } from "vue"
-import { reactiveOmit } from "@vueuse/core"
-import { TabsList } from "reka-ui"
-import { cn } from "@/lib/utils"
+import type { TabsListProps } from 'reka-ui'
+import { TabsList } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+import { cn } from '@/lib/utils'
 
 const props = defineProps<TabsListProps & { class?: HTMLAttributes["class"] }>()
 
@@ -15,7 +15,7 @@ const delegatedProps = reactiveOmit(props, "class")
     data-slot="tabs-list"
     v-bind="delegatedProps"
     :class="cn(
-      'bg-muted text-muted-foreground inline-flex h-9 w-fit items-center justify-center rounded-lg p-[3px]',
+      'bg-muted text-muted-foreground flex h-9 w-full items-center justify-center rounded-lg p-[3px]',
       props.class,
     )"
   >
