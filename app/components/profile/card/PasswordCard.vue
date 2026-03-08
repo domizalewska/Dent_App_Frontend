0ff41441c8ea86c2f21bc4c02b9a8625a0ee2361
Dominika <domzal1801@gmail.com>
2026-03-08 13:28:12 +0100

    feat: Created components for profile card and password

---
+++ app/components/profile/card/PasswordCard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts>">
+import BaseCard from '~/components/base/card/BaseCard.vue'
+</script>
+
+<template>
+  <BaseCard card-title="Zmień hasło" card-buttons class="mt-2">
+    <template #content>
+      <div class="grid gap-3">
+        <Label for="tabs-demo-name">Hasło</Label>
+        <Input id="tabs-demo-name" default-value="Pedro Duarte" />
+      </div>
+      <div class="grid gap-3">
+        <Label for="tabs-demo-username">Nowe hasło</Label>
+        <Input id="tabs-demo-username" default-value="@peduarte" />
+      </div>
+      <div class="grid gap-3">
+        <Label for="tabs-demo-username">Powtórz nowe hasło</Label>
+        <Input id="tabs-demo-username" default-value="@peduarte" />
+      </div>
+    </template>
+  </BaseCard>
+</template>
--- app/components/profile/card/ProfileCard.vue
+++ app/components/profile/card/ProfileCard.vue
@@ -1,25 +1,22 @@
-<script setup lang="ts"></script>
-
+<script setup lang="ts>"></script>
 <template>
-  <Card>
-    <CardHeader>
-      <CardTitle>Account</CardTitle>
-      <CardDescription>
-        Make changes to your account here. Click save when you're done.
-      </CardDescription>
-    </CardHeader>
-    <CardContent class="grid gap-6">
+  <BaseCard card-title="Dane użytkownika" card-buttons class="mt-2">
+    <template #content>
       <div class="grid gap-3">
-        <Label for="tabs-demo-name">Name</Label>
-        <Input id="tabs-demo-name" default-value="Pedro Duarte" />
+        <Label for="tabs-demo-name">Imię</Label>
+        <Input id="tabs-demo-name" default-value="Pedro" />
       </div>
       <div class="grid gap-3">
-        <Label for="tabs-demo-username">Username</Label>
-        <Input id="tabs-demo-username" default-value="@peduarte" />
+        <Label for="tabs-demo-username">Nazwisko</Label>
+        <Input id="tabs-demo-username" default-value="Duarte" />
       </div>
-    </CardContent>
-    <CardFooter>
-      <Button>Save changes</Button>
-    </CardFooter>
-  </Card>
+      <div class="grid gap-3">
+        <Label for="tabs-demo-username">Email</Label>
+        <Input id="tabs-demo-username" default-value="peduarte@gmail.com" />
+      </div>
+    </template>
+    <template #footer>
+      <Button class="flex justify-end mt-4">Zapisz zmiany</Button>
+    </template>
+  </BaseCard>
 </template>
