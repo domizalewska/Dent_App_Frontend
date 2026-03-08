0400c9df16d64062de32a953f1568f65b8a2b4a8
Dominika <domzal1801@gmail.com>
2026-03-08 12:13:53 +0100

    fix: Fixed eslint config options

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,17 +3,7 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default withNuxt([eslintPluginPrettierRecommended], {
-  ignore: [
-    '.github/**',
-    'tsconfig.json',
-    'nuxt.config.ts',
-    'package.json',
-    'package-lock.json',
-    'dist/**',
-    '.nuxt/**',
-    'node_modules/**',
-    'app/components/ui/**',
-  ],
+  ignores: ['app/components/ui/**'],
   rules: {
     'node/prefer-global/process': 'off',
   },
