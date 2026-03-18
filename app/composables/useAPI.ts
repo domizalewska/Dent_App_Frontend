d2d9100addf5287ce8af0e528af226d39ffa32d7
Dominika <domzal1801@gmail.com>
2026-03-18 18:56:28 +0100

    feat: Created paginated types and api

--- app/composables/useAPI.ts
+++ app/composables/useAPI.ts
@@ -1,7 +1,18 @@
 import { useFetch, type UseFetchOptions, useNuxtApp } from 'nuxt/app'
+import type { PaginatedResponse } from '~/types/api/PaginationTypes'
 
 export function useAPI<T>(url: string | (() => string), options: UseFetchOptions<T> = {}) {
   return useFetch(url, {
+    ...options,
+    $fetch: useNuxtApp().$api as typeof $fetch,
+  })
+}
+
+export function usePaginatedAPI<T>(
+  url: string | (() => string),
+  options: UseFetchOptions<PaginatedResponse<T>> = {},
+) {
+  return useFetch<PaginatedResponse<T>>(url, {
     ...options,
     $fetch: useNuxtApp().$api as typeof $fetch,
   })
---
+++ app/types/api/PaginationTypes.ts
@@ -0,0 +1,19 @@
+export type PaginatedResult<T> = {
+  current_page: number
+  data: T
+  from: number
+  last_page: number
+  per_page: number
+  to: number
+  total: number
+}
+
+export type PaginatedResponse<T> = {
+  data: T
+  current_page: number
+  from: number
+  last_page: number
+  per_page: number
+  to: number
+  total: number
+}
