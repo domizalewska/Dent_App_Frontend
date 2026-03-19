ea79d5188df3c43dfc0f392b240697248880a53d
Dominika <domzal1801@gmail.com>
2026-03-19 21:44:17 +0100

    feat: Created new structure for project to get better type

---
+++ app/features/users/users.endpoints.ts
@@ -0,0 +1,4 @@
+export const UsersEndpoints = {
+  LIST: '/user/selectlist',
+  USER_DETAILS: '/api/user',
+} as const
--- app/types/endpoints.ts
+++
@@ -1,6 +0,0 @@
-//USERS
-USER_LIST = '/user/selectlist'
-USER = '/user'
-
-//JOB
-JOB = '/job-position'
