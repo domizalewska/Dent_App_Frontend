acf2f8f71a7835f087044df41cc36d6830ce8a4b
Dominika <domzal1801@gmail.com>
2026-03-25 19:46:11 +0100

    feat: Created custom fetch to add headers and use response on 401

---
+++ app/plugins/api.ts
@@ -0,0 +1,26 @@
+export default defineNuxtPlugin((nuxtApp) => {
+  const apiUrl = useRuntimeConfig().public.apiBase
+  const { logoutUser } = useAuth()
+
+  const api = $fetch.create({
+    baseURL: apiUrl,
+    onRequest({ options }) {
+      const token = localStorage.getItem('token')
+      if (token) {
+        options.headers.set('Authorization', `Bearer ${token}`)
+      }
+    },
+    async onResponseError({ response }) {
+      if (response.status === 401) {
+        logoutUser()
+        await nuxtApp.runWithContext(() => navigateTo('/login'))
+      }
+    },
+  })
+
+  return {
+    provide: {
+      api,
+    },
+  }
+})
