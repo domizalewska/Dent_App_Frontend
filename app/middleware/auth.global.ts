fcffcbb63a724a452c279f0d2a4d007d6e69f034
Dominika <domzal1801@gmail.com>
2026-04-01 21:37:17 +0200

    fix: Fixed middleware

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -3,8 +3,8 @@ import { useAuth } from '~/composables/useAuth'
 export default defineNuxtRouteMiddleware((to) => {
   const isAuthenticated = useAuth()
 
-  const publicPages = ['/login', '/register', '/forgot-password']
-  if (!isAuthenticated && publicPages.includes(to.path)) {
+  const publicPages = ['/', '/login', '/register', '/forgot-password']
+  if (!isAuthenticated.isAuthenticated.value && !publicPages.includes(to.path)) {
     return navigateTo('/login')
   }
 })
