3d7aa6b24af3d7330dad00dbf77d542deb8bb83e
Dominika <domzal1801@gmail.com>
2026-04-02 22:58:47 +0200

    feat: Added new component skeleton

---
+++ app/components/base/skeleton/BaseDatatableSkeleton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Skeleton } from '@/components/ui/skeleton'
+</script>
+
+<template>
+  <div class="flex w-full h-screen flex-col gap-2">
+    <div
+      v-for="index in 5"
+      :key="index"
+      class="flex gap-4"
+    >
+      <Skeleton class="h-4 flex-1" />
+      <Skeleton class="h-4 w-35" />
+      <Skeleton class="h-4 w-35" />
+      <Skeleton class="h-4 w-35" />
+    </div>
+  </div>
+</template>
