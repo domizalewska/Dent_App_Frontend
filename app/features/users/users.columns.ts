37d8f9cb76ddcf54ae2b412d970cbd2f3e2e90d0
Dominika <domzal1801@gmail.com>
2026-04-07 20:59:22 +0200

    feat: Added new component for badge and fix users table

--- app/features/users/users.columns.ts
+++ app/features/users/users.columns.ts
@@ -1,7 +1,10 @@
 import { h } from 'vue'
+import { Trash2 } from 'lucide-vue-next'
 import type { ColumnDef } from '@tanstack/vue-table'
 import { formatDateToString } from '~/utils/formatDate'
 import type { User } from '~/types'
+import BaseBadge from '~/components/base/badge/BaseBadge.vue'
+import { Button } from '~/components/ui/button'
 
 export const usersColumns: ColumnDef<User>[] = [
   {
@@ -23,6 +26,14 @@ export const usersColumns: ColumnDef<User>[] = [
     },
   },
   {
+    accessorKey: 'work_email',
+    header: () =>
+      h('div', { class: 'flex item-center text-center text-foreground' }, 'Email pracowniczy'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-normal text-muted-foreground' }, row.original.email)
+    },
+  },
+  {
     accessorKey: 'phone',
     header: () => h('div', { class: 'flex item-center text-center text-foreground' }, 'Telefon'),
     cell: ({ row }) => {
@@ -39,6 +50,42 @@ export const usersColumns: ColumnDef<User>[] = [
         'div',
         { class: 'text-left font-normal text-muted-foreground' },
         formatDateToString(row.original.created_at, 'dd-MM-yyyy'),
+      )
+    },
+  },
+  {
+    accessorKey: 'is_active',
+    header: () =>
+      h('div', { class: 'flex item-center text-center text-foreground' }, 'Użytkownik aktywny'),
+    cell: ({ row }) => {
+      return h(
+        'div',
+        { class: ' text-left font-normal text-muted-foreground' },
+        row.original.is_active
+          ? h(BaseBadge, {
+              label: 'Aktywny',
+              variant: 'secondary',
+              badgeClass: 'bg-green-500 text-white dark:bg-green-600',
+            })
+          : h(BaseBadge, {
+              label: 'Nieaktywny',
+              badgeClass: 'bg-red-500 text-white dark:bg-red-600',
+            }),
+      )
+    },
+  },
+  {
+    accessorKey: 'action',
+    header: () => h('div', { class: 'flex item-center text-center text-foreground' }),
+    cell: ({ row }) => {
+      return h(
+        'div',
+        { class: 'text-left font-normal text-muted-foreground' },
+        h(
+          Button,
+          { variant: 'outline', size: 'icon', class: 'rounded-full' },
+          { default: () => h(Trash2, { class: 'size-4' }) },
+        ),
       )
     },
   },
--- app/types/user/user.type.ts
+++ app/types/user/user.type.ts
@@ -3,6 +3,7 @@ export interface User {
   first_name: string
   last_name: string
   email: string
+  work_email: string
   phone: string
   created_at: Date
   is_active?: boolean
