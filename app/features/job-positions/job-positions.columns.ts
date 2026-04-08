b93128d2de28f6a21dfb0482218937784cf75c82
Dominika <domzal1801@gmail.com>
2026-04-08 20:58:21 +0200

    fix: Fixed columns name for table

--- app/features/job-positions/job-positions.columns.ts
+++ app/features/job-positions/job-positions.columns.ts
@@ -5,14 +5,15 @@ import type { JobPosition } from '~/types/job-position/job-position.type'
 export const jobPositionsColumns: ColumnDef<JobPosition>[] = [
   {
     accessorKey: 'name',
-    header: () => h('div', { class: 'flex item-center text-center text-foreground' }, 'Użytkownik'),
+    header: () => h('div', { class: 'flex item-center text-center text-foreground' }, 'Stanowisko'),
     cell: ({ row }) => {
       return h('div', { class: 'text-left font-normal text-muted-foreground' }, row.original.name)
     },
   },
   {
     accessorKey: 'f_name',
-    header: () => h('div', { class: 'flex item-center text-center text-foreground' }, 'Email'),
+    header: () =>
+      h('div', { class: 'flex item-center text-center text-foreground' }, 'Nazwa żeńska'),
     cell: ({ row }) => {
       return h('div', { class: 'text-left font-normal text-muted-foreground' }, row.original.f_name)
     },
@@ -20,7 +21,7 @@ export const jobPositionsColumns: ColumnDef<JobPosition>[] = [
   {
     accessorKey: 'm_name',
     header: () =>
-      h('div', { class: 'flex item-center text-center text-foreground' }, 'Email pracowniczy'),
+      h('div', { class: 'flex item-center text-center text-foreground' }, 'Nazwa męska'),
     cell: ({ row }) => {
       return h('div', { class: 'text-left font-normal text-muted-foreground' }, row.original.m_name)
     },
