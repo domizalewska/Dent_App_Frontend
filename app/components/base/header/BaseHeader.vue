e2a68b42a4eac63a94622cc53184c0566748f805
Dominika <domzal1801@gmail.com>
2026-04-09 23:17:46 +0200

    feat: Created new component for export files

---
+++ app/components/base/header/BaseHeader.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+</script>
+<template>
+  <div
+class="flex flex-col gap-1 overflow-auto border border-border rounded-xl mb-4
+">
+    <div>
+      <slot name="top" />
+    </div>
+    <div class="flex flex-row justify-between flex-wrap px-4 gap-2">
+      <div>
+        <slot name="left" />
+      </div>
+      <div>
+        <slot name="right" />
+      </div>
+    </div>
+    <div>
+      <slot name="bottom" />
+    </div>
+  </div>
+</template>
