6f04b786b3a3e604819bd6c370c769909bf818fc
Dominika <domzal1801@gmail.com>
2026-04-16 21:46:17 +0200

    fix: Fixed header page

--- app/components/page/HeaderPage.vue
+++ app/components/page/HeaderPage.vue
@@ -12,7 +12,7 @@ const props = defineProps<Props>()
       <h2 class="scroll-m-20 text-3xl font-semibold text-foreground tracking-tight">
         {{ props.title }}
       </h2>
-      <div id="header-page-buttons" class="flex items-center gap-2" />
+      <div id="header-page-buttons" class="flex items-center gap-2 mb-2" />
     </div>
   </div>
 </template>
