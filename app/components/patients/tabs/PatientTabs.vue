0d29e2ecdcfa85b25519c5b9d310c64dd441fe2b
Dominika <domzal1801@gmail.com>
2026-04-25 09:37:32 +0200

    feat: Created component for patient tab

---
+++ app/components/patients/tabs/PatientTabs.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+import type { User } from '~/types'
+
+interface Props {
+  user: User
+}
+
+defineProps<Props>()
+
+const tabs = [
+  {
+    name: 'Informacje',
+    value: 'info',
+    icon: 'solar:user-id-line-duotone',
+  },
+  {
+    name: 'Dokumenty',
+    value: 'documents',
+    icon: 'material-symbols:shield-outline',
+  },
+  {
+    name: 'Powiadomienia',
+    value: 'notifications',
+    icon: 'solar:user-id-line-duotone',
+  },
+]
+</script>
+
+<template>
+  <Tabs default-value="info" class="w-full gap-0">
+    <Card class="overflow-hidden pt-0 rounded-b-none border-b-0">
+      <div
+        class="h-32 bg-muted bg-cover bg-center sm:h-40"
+        style="
+          background-image: url('https://deifkwefumgah.cloudfront.net/shadcnblocks/block/photos/pawel-czerwinski-O4fAgtXLRwI-unsplash.jpg');
+        "
+      />
+      <CardContent class="relative px-6 pb-0">
+        <ProfileHeaderComponent :user="user" />
+      </CardContent>
+      <div class="px-6 pt-2">
+        <TabsList class="w-full">
+          <TabsTrigger
+            v-for="tab in tabs"
+            :key="tab.name"
+            :value="tab.value"
+            class="flex items-center gap-1 px-2.5 sm:px-3"
+          >
+            <Icon :icon="tab.icon" />
+            {{ tab.name }}
+          </TabsTrigger>
+        </TabsList>
+      </div>
+    </Card>
+
+    <TabsContent value="info" class="mt-0">
+      <PatientInfoTab />
+    </TabsContent>
+    <TabsContent value="documents" class="mt-0">
+      <PasswordCard />
+    </TabsContent>
+    <TabsContent value="notifications" class="mt-0" />
+  </Tabs>
+</template>
