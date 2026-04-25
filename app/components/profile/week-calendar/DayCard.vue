8bcd8742881d63f72f64a07a8980a7d5e130f21d
Dominika <domzal1801@gmail.com>
2026-04-25 18:19:23 +0200

    feat: Created card for week calendar view

---
+++ app/components/profile/week-calendar/DayCard.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+interface Props {
+  dayOfWeek: number
+  nameOfWeek: string
+  slotNumber: number
+}
+
+defineProps<Props>()
+</script>
+<template>
+  <Button class="flex flex-1 w-[150px] h-[150px] flex-col items-center" variant="outline">
+    <slot>
+      <span class="font-medium">{{ nameOfWeek }}</span>
+      <span>{{ dayOfWeek }}</span>
+      <span class="mt-0.5 text-xs tabular-nums opacity-70">{{ slotNumber }}</span>
+    </slot>
+  </Button>
+</template>
