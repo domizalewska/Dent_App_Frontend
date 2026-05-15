7dba4dc3c9cd1db4894efdbdeeea29c30c8edf7b
Dominika <domzal1801@gmail.com>
2026-05-15 13:53:42 +0200

    feat: Created new CSS file for calendar module

--- app/pages/schedule/index.vue
+++ app/pages/schedule/index.vue
@@ -3,6 +3,7 @@ import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
+import plLocale from '@fullcalendar/core/locales/pl'
 import { useBreadcrumbs } from '~/composables/useBreadcrumbs.ts'
 import { useHeader } from '~/composables/useHeader.ts'
 
@@ -11,22 +12,46 @@ definePageMeta({
 })
 
 const { set } = useBreadcrumbs()
-
 set([{ name: 'Kalendarz', link: '/schedule' }])
 
 const { setHeader, resetHeader } = useHeader()
-
 resetHeader()
 setHeader('Kalendarz')
 
 const options = {
   plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
   initialView: 'timeGridWeek',
+  locale: plLocale,
+  allDaySlot: false,
   editable: true,
+  selectable: true,
+  selectMirror: true,
+  slotMinTime: '07:00:00',
+  slotMaxTime: '21:00:00',
+  slotDuration: '00:15:00',
+  slotLabelInterval: '01:00:00',
+  dayHeaderFormat: { weekday: 'short' },
+  slotLabelFormat: {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  },
+  headerToolbar: {
+    left: 'prev,next today',
+    center: 'title',
+    right: 'timeGridWeek,dayGridMonth',
+  },
+  buttonText: {
+    today: 'Dziś',
+    week: 'Tydzień',
+    month: 'Miesiąc',
+  },
   events: [{ title: 'Jan Kowalski', start: '2025-01-20T08:00:00', end: '2025-01-20T16:00:00' }],
 }
 </script>
 
 <template>
-  <FullCalendar :options="options" />
+  <div class="fc-wrapper">
+    <FullCalendar :options="options" />
+  </div>
 </template>
