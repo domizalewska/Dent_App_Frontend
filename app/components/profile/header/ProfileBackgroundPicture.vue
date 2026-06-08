a97f131a474672f426bcd98783ec4f1c9152b1da
Dominika <domzal1801@gmail.com>
2026-06-08 21:07:15 +0200

    fix: Fixed style in ProfileBackgroundPicture.vue

--- app/components/profile/header/ProfileBackgroundPicture.vue
+++ app/components/profile/header/ProfileBackgroundPicture.vue
@@ -34,7 +34,9 @@ function handleBackgroundAction() {
 <template>
   <div
     class="group relative flex h-32 bg-muted bg-cover bg-center sm:h-40"
-    :style="{ backgroundImage: `url('${user.background_picture ?? 'https://deifkwefumgah.cloudfront.net/shadcnblocks/block/photos/pawel-czerwinski-O4fAgtXLRwI-unsplash.jpg'}')` }"
+    :style="{
+      backgroundImage: `url('${user.background_picture ?? 'https://deifkwefumgah.cloudfront.net/shadcnblocks/block/photos/pawel-czerwinski-O4fAgtXLRwI-unsplash.jpg'}')`,
+    }"
   >
     <button
       class="absolute inset-0 flex items-center justify-center bg-black/40 opacity-0 transition-opacity group-hover:opacity-100"
