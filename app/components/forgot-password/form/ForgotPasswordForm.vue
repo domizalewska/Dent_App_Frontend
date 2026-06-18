52aa007a627f9f61cfa0b4fe4e6d596068f75326
Dominika <domzal1801@gmail.com>
2026-06-18 10:13:59 +0200

    fix: Fixed forgot password form

--- app/components/forgot-password/form/ForgotPasswordForm.vue
+++ app/components/forgot-password/form/ForgotPasswordForm.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
 import { toTypedSchema } from '@vee-validate/zod'
-import { Field as VeeField, useForm } from 'vee-validate'
+import { Form } from 'vee-validate'
 import { z } from 'zod'
-
-import { Input } from '~/components/ui/input'
 import { Button } from '~/components/ui/button'
 import {
   Card,
@@ -13,7 +11,6 @@ import {
   CardHeader,
   CardTitle,
 } from '~/components/ui/card'
-import { Field, FieldError, FieldGroup, FieldLabel } from '~/components/ui/field'
 import { useAPI } from '~/composables/useAPI'
 
 const router = useRouter()
@@ -24,20 +21,17 @@ const formSchema = toTypedSchema(
   }),
 )
 
-const { handleSubmit } = useForm({
-  validationSchema: formSchema,
-  initialValues: {
-    email: '',
-  },
-})
+const initialValues = {
+  email: '',
+}
 
-const onSubmit = handleSubmit(async (values) => {
+async function onSubmit(values: FormData) {
   await useAPI('/forgot-password', {
     method: 'POST',
     body: values,
   })
   await router.push('forgot-password/detail')
-})
+}
 </script>
 
 <template>
@@ -50,23 +44,12 @@ const onSubmit = handleSubmit(async (values) => {
     </CardHeader>
 
     <CardContent>
-      <form @submit="onSubmit">
-        <FieldGroup>
-          <VeeField v-slot="{ field, errors }" name="email">
-            <Field :data-invalid="!!errors.length">
-              <FieldLabel>Email</FieldLabel>
-
-              <Input
-                v-bind="field"
-                type="email"
-                placeholder="Email"
-                :aria-invalid="!!errors.length"
-              />
-
-              <FieldError v-if="errors.length" :errors="errors" />
-            </Field>
-          </VeeField>
-        </FieldGroup>
+      <Form :validation-schema="formSchema" :initial-values="initialValues" @submit="onSubmit">
+        <BaseInputForm
+          name="email"
+          label="E-mail"
+          placeholder="Wpisz adres e-mail do odzyskania hasła"
+        />
 
         <CardFooter class="flex flex-col mt-4">
           <Button class="w-full mb-4" type="submit"> Zresetuj hasło </Button>
@@ -77,7 +60,7 @@ const onSubmit = handleSubmit(async (values) => {
             </div>
           </NuxtLink>
         </CardFooter>
-      </form>
+      </Form>
     </CardContent>
   </Card>
 </template>
