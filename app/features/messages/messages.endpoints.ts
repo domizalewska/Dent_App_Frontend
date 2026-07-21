f9f14f04e743f50e78811a83c18240c25f9e5e4b
Dominika <domzal1801@gmail.com>
2026-07-21 20:32:27 +0200

    feat: Added new endpoints for messages module

--- app/features/messages/messages.endpoints.ts
+++ app/features/messages/messages.endpoints.ts
@@ -2,14 +2,18 @@ export const MessagesEndpoints = {
   BASE: '/message',
   DETAILS: (uuid: string) => `/message/${uuid}`,
   FILES: (messageUuid: string) => `/message/${messageUuid}/file`,
-  FILE_DETAILS: (messageUuid: string, fileUuid: string) => `/message/${messageUuid}/file/${fileUuid}`,
-  FILE_DOWNLOAD: (messageUuid: string, fileUuid: string) => `/message/${messageUuid}/file-download/${fileUuid}`,
+  FILE_DETAILS: (messageUuid: string, fileUuid: string) =>
+    `/message/${messageUuid}/file/${fileUuid}`,
+  FILE_DOWNLOAD: (messageUuid: string, fileUuid: string) =>
+    `/message/${messageUuid}/file-download/${fileUuid}`,
 }
 
 export const MessageGroupsEndpoints = {
   BASE: '/message-group',
   DETAILS: (uuid: string) => `/message-group/${uuid}`,
   MESSAGES: (uuid: string) => `/message-group/${uuid}/messages`,
-  ADD_USER: (groupUuid: string, userUuid: string) => `/message-group/${groupUuid}/users/${userUuid}`,
-  REMOVE_USER: (groupUuid: string, userUuid: string) => `/message-group/${groupUuid}/users/${userUuid}`,
+  ADD_USER: (groupUuid: string, userUuid: string) =>
+    `/message-group/${groupUuid}/users/${userUuid}`,
+  REMOVE_USER: (groupUuid: string, userUuid: string) =>
+    `/message-group/${groupUuid}/users/${userUuid}`,
 }
