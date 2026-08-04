3c3a36f2609793cc8981ecaa049562a28b296951
Dominika <domzal1801@gmail.com>
2026-08-04 19:53:14 +0200

    feat: Added new component of settings button to schedule

---
+++ app/components/schedule/button/ScheduleSettingButton.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { Settings, Repeat, Link2 } from 'lucide-vue-next'
+
+const emit = defineEmits<{
+  'open-pattern': []
+  'open-google': []
+}>()
+</script>
+
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button variant="outline" size="icon" aria-label="Ustawienia grafiku">
+        <Settings class="size-4" />
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end" class="w-56">
+      <DropdownMenuItem @click="emit('open-pattern')">
+        <Repeat class="mr-2 size-4" />
+        Wzorzec tygodniowy
+      </DropdownMenuItem>
+      <DropdownMenuItem @click="emit('open-google')">
+        <Link2 class="mr-2 size-4" />
+        Integracja z Google Calendar
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
